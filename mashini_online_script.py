d221088142cdf03edfae34de08a36b62ab4ce94e
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2023-05-10 11:51:57 +0300

    created function to add the products in dict with their characterististics

--- mashini_online_script.py
+++ mashini_online_script.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import openpyxl
 
 
 def get_categories_links():
@@ -13,9 +14,9 @@ def get_categories_links():
     list_of_links = []
 
     for link in links:
-        new_link = str(link).split(' ')
-        new_new_link = str(new_link[3]).split('"')
-        list_of_links.append('https://www.onlinemashini.bg' + str(new_new_link[1]))
+        split_link_1 = str(link).split(' ')
+        split_link_2 = str(split_link_1[3]).split('"')
+        list_of_links.append('https://www.onlinemashini.bg' + str(split_link_2[1]))
 
     return list_of_links
 
@@ -27,40 +28,86 @@ def get_products_links():
 
     for url in urls:
 
-        if len(list_of_products_link) > 2:
+        if len(list_of_products_link) > 1:
             break
 
-        response = requests.get(url)
+        response = requests.get("https://www.onlinemashini.bg/cat/22/klimatizaciq-ventilaciq-otoplenie/82/kaloriferi-elektri4eski-trifazni")
         doc = BeautifulSoup(response.content, 'html.parser')
 
         pages = doc.find('ul', {'class': 'pagination'})
 
-        split_1 = str(pages).split("<a")
-        split_2 = str(split_1[-1]).split('"')
-        split_3 = str(split_2[1]).split("/")
-        number = int(split_3[-1])
+        if pages != None:
+            split_1 = str(pages).split("<a")
+            split_2 = str(split_1[-1]).split('"')
+            split_3 = str(split_2[1]).split("/")
+            number = int(split_3[-1])
 
-        for page in range(1, number + 1):
+            for page in range(1, number + 1):
 
-            if len(list_of_products_link) > 2:
-                break
+                if len(list_of_products_link) > 1:
+                    break
 
-            paged_url = f'{url}/{page}'
-            response = requests.get(paged_url).text
-            doc = BeautifulSoup(response, 'html.parser')
+                paged_url = f'{url}/{page}'
+                response = requests.get(paged_url).text
+                doc = BeautifulSoup(response, 'html.parser')
 
+                for link in doc.find_all('div', {'class': 'col-8'}):
+                    split_11 = str(link).split('href="/')
+                    split_12 = str(split_11[1]).split('"')
+                    product_link = f'https://www.onlinemashini.bg/{str(split_12[0])}'
+                    list_of_products_link.append(product_link)
+
+                    if len(list_of_products_link) > 1:
+                        break
+        elif pages:
             for link in doc.find_all('div', {'class': 'col-8'}):
                 split_11 = str(link).split('href="/')
                 split_12 = str(split_11[1]).split('"')
                 product_link = f'https://www.onlinemashini.bg/{str(split_12[0])}'
                 list_of_products_link.append(product_link)
 
-                if len(list_of_products_link) > 2:
+                if len(list_of_products_link) > 1:
                     break
 
-
+        else:
+            break
 
     return list_of_products_link
 
 
-print(get_products_links())
+def get_product_info():
+    products = {}
+    list_of_products_urls = get_products_links()
+
+    for url in list_of_products_urls:
+        response = requests.get(url)
+        doc = BeautifulSoup(response.content, 'html.parser')
+        name = doc.find(class_='product-name').text.strip()
+        price = int(doc.find(class_='price mb-2 mt-1').text.strip('лв.'))/100
+
+        product_info = doc.find('div', {'class': 'decs details'})
+        product_characteristics = product_info.get_text(strip=True, separator="\n")
+
+        if name not in products:
+            products[name] = [f'{price:.2f}', str(product_characteristics)]
+
+    return products
+
+
+workbook = openpyxl.Workbook()
+
+worksheet = workbook.active
+
+worksheet['A1'] = 'Product Name'
+worksheet['B1'] = 'Price'
+worksheet['C1'] = 'Info'
+
+row = 2
+for product_name, data in get_product_info().items():
+    worksheet.cell(row=row, column=1, value=product_name)
+    worksheet.cell(row=row, column=2, value=data[0])
+    worksheet.cell(row=row, column=3, value=data[1])
+    row += 1
+
+
+workbook.save('products.xlsx')
